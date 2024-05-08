444996f87706cac0bf27d85ae13e34a5f54c09ce
Ayan Maiti <mightyayan369@gmail.com>
2024-05-09 00:27:00 +0530

    Add error handling and Json Validation

---
+++ Error.cs
@@ -0,0 +1,11 @@
+
+using System;
+using Microsoft.VisualBasic;
+using Moonman.DBRecordTypes;
+
+namespace Moonman.Errors {
+    public class Error {
+        public string ErrorDescription {get; set;}
+        public Object QueueMessage {get; set;}
+    } 
+}
--- Sync-Queue-DB.cs
+++ Sync-Queue-DB.cs
@@ -12,6 +12,10 @@ using Microsoft.Azure.Functions.Worker.Extensions.Sql;
 using Microsoft.Data.SqlClient;
 using Microsoft.IdentityModel.Protocols;
 using System.Configuration;
+using Moonman.Errors;
+using Microsoft.VisualBasic;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
 
 
 
@@ -21,17 +25,34 @@ namespace Moonman.Function
     public class Sync_Queue_DB
     {
         readonly string queueConnectionString = System.Environment.GetEnvironmentVariable("QueueConnectionString", EnvironmentVariableTarget.Process);
+        readonly string errorQueueConnectionString = System.Environment.GetEnvironmentVariable("ErrorQueueConnectionString");
         readonly string queueName = "genesis-queue-0";
-        
+        readonly string errorQueueName = "genesis-error-00";
+        readonly string sqlConnectionString = System.Environment.GetEnvironmentVariable("SqlConnectionString", EnvironmentVariableTarget.Process);
+        readonly JSchema schema = JSchema.Parse(
+            @"{
+                'title': 'Order',
+                'type': 'object',
+                'properties': {
+                    'OrderId': {'type': 'string', required: true},
+                    'Quantity': {'type': 'integer', required: true},
+                    'UnitPrice': {type: 'number', required: true}
+                },
+                'additionalProperties': false
+            }"
+        );
 
 
         [FunctionName("Sync_Queue_DB")]
         public async Task Run([TimerTrigger("0 */40 * * * *")]TimerInfo myTimer, ILogger log)
         {
+            var currentMessage = new Object();
             if (myTimer.IsPastDue) {
                 log.LogInformation("Timer is running late!");
             }
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            ServiceBusClient serviceBusErrorClient = new(errorQueueConnectionString);
+            ServiceBusSender serviceBusSender = serviceBusErrorClient.CreateSender(errorQueueName);
             try
             {
                 ServiceBusClient serviceBusClient = new (queueConnectionString);
@@ -42,23 +63,33 @@ namespace Moonman.Function
 
 
 
-                SqlConnection connection = new(System.Environment.GetEnvironmentVariable("SqlConnectionString", EnvironmentVariableTarget.Process));
+                SqlConnection connection = new(sqlConnectionString);
                 connection.Open();
                 await foreach (var message in messages) {
+                    currentMessage = message;
                     count++;
-                    Order order = JsonConvert.DeserializeObject<Order>(message.Body.ToString());
-                    OrderOutput orderOutput = new() {
-                        Order = order
-                    };
+                    JObject jsonObj = JObject.Parse(message.Body.ToString());
+                    log.LogInformation(jsonObj.IsValid(schema).ToString());
+                    
+                    if (jsonObj.IsValid(schema)) {
+                        Order order = JsonConvert.DeserializeObject<Order>(message.Body.ToString());
+                        OrderOutput orderOutput = new() {
+                            Order = order
+                        };
 
-                    string insertQuery = "INSERT INTO Orders (OrderId, Quantity, UnitPrice) VALUES (@Value1, @Value2, @Value3)";
+                        string insertQuery = "INSERT INTO Orders (OrderId, Quantity, UnitPrice) VALUES (@Value1, @Value2, @Value3)";
 
-                        
-                        SqlCommand sqlCommand = new (insertQuery, connection);
-                        sqlCommand.Parameters.AddWithValue("@Value1", order.OrderId);
-                        sqlCommand.Parameters.AddWithValue("@Value2", order.Quantity);
-                        sqlCommand.Parameters.AddWithValue("@Value3", order.UnitPrice);
-                        using SqlDataReader reader = sqlCommand.ExecuteReader();
+                            
+                            SqlCommand sqlCommand = new (insertQuery, connection);
+                            sqlCommand.Parameters.AddWithValue("@Value1", order.OrderId);
+                            sqlCommand.Parameters.AddWithValue("@Value2", order.Quantity);
+                            sqlCommand.Parameters.AddWithValue("@Value3", order.UnitPrice);
+                            using SqlDataReader reader = sqlCommand.ExecuteReader();
+                    }
+                    else {
+                        ServiceBusMessage serviceBusMessage = new ServiceBusMessage(JsonConvert.SerializeObject(message));
+                        await serviceBusSender.SendMessageAsync(serviceBusMessage);
+                    }
                         
                     
                     
@@ -70,6 +101,12 @@ namespace Moonman.Function
             catch (System.Exception e)
             {
                 log.LogError(e.ToString());
+                // Error err = new() {
+                //     ErrorDescription = e.ToString(),
+                //     QueueMessage = currentMessage
+                // };
+                // ServiceBusMessage serviceBusMessage = new ServiceBusMessage(JsonConvert.SerializeObject(err));
+                // await serviceBusSender.SendMessageAsync(serviceBusMessage);
             }
         }
     }
